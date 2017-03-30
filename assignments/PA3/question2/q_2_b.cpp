0a3a09c12865af104851ac609db06303cb33ffd8
jaskirat1208 <jaskiratsingh1208@gmail.com>
2017-03-31 01:02:56 +0530

    traversal done

--- assignments/PA3/question2/q_2_b.cpp
+++ assignments/PA3/question2/q_2_b.cpp
@@ -2,61 +2,66 @@
 using namespace std;
 void traverse_seg_tree(long long int i,long long int j, long long int start,long long int end, long long int a, long long int d ){
 	if(i==start && j==end){
-		// cout<<start<<" "<<end;
-		// return 0;
+		cout<<start<<" "<<end<<endl;
+		return ;
 	}	
 	// cout<<start<<" "<<end;
 	int size_of_zero=(end-start+2)/3;
 	int size_of_one=(end-start+1)/3;
 	int size_of_two=(end-start)/3;
-	cout<<size_of_two<<size_of_one<<size_of_zero;
-	if (start+size_of_two>i)																					//i is in first segment
+	// cout<<size_of_two<<size_of_one<<size_of_zero;
+	if (i>=start && i <= start+size_of_two-1)//i is in first segment
 	{
-		cout<<"i is in first segment"<<" ";
-		if (start + size_of_two > j)																				//j is in first segment
-		{
-			cout<<"j is in 2 segment"<<endl;
-			// traverse_seg_tree(i,j,start,start+size_of_two,a,d*3);	
+		cout<<"i in first";
+		if(j>=start && j < start+size_of_two-1){
+			cout<<"j in first";
+			traverse_seg_tree(i,j,start,start+size_of_two-1,0,0);
 		}
-		if (start + size_of_two < j && start + size_of_two + size_of_one > j)  										//j is in second segment
+		else if(j >=start+size_of_two && j <= start+size_of_two+size_of_one-1)
 		{
-			cout<<"j is in 2 segment"<<endl;
-			// traverse_seg_tree(i,start+size_of_two,start,start+size_of_two,a,d*3);
-			// traverse_seg_tree(start+size_of_two,j,start+size_of_two,start+size_of_one,a,d*3);	
+			cout<<"j in sec";
+			traverse_seg_tree(i,start+size_of_two-1,start,start+size_of_two-1,0,0);
+			traverse_seg_tree(start+size_of_two,j,start+size_of_two,start+size_of_two+size_of_one-1,0,0);
 		}
-		if (start + size_of_two +size_of_one + size_of_zero < j && start + size_of_two + size_of_one > j)			//j is in third segment
+		else if (j >=start+size_of_two+size_of_one && j <=end - 1 )
 		{
-			cout<<"j is in 2 segment"<<endl;
-			// traverse_seg_tree(i,start+size_of_two,start,start+size_of_two,a,d*3);
-			// traverse_seg_tree(start+size_of_two, start+size_of_two+size_of_one, start, start+size_of_two, a, d*3);
-			// traverse_seg_tree(start+size_of_two+size_of_one, j, start+size_of_two+size_of_one, end, a, 3*d);	
+			cout<<"j in third";
+			traverse_seg_tree(i,start+size_of_two-1,start,start+size_of_two-1,0,0);
+			traverse_seg_tree(start+size_of_two,start+size_of_two+size_of_one-1,start+size_of_two,start+size_of_two+size_of_one-1,0,0);
+			traverse_seg_tree(start+size_of_two+size_of_one,j,start+size_of_two+size_of_one,end -1 ,0,0);	
 		}
 	}
-	if(start+size_of_two <= i && start+size_of_two+size_of_one > i)
+	// i is in second segment
+	if (i >=start+size_of_two && i <= start+size_of_two+size_of_one-1)
 	{
-		cout<<"i is in 2 segment"<<endl;
-		if(start+size_of_two+size_of_one <= j && start+size_of_two > j)												//j is in second segment
+		cout<<"i in second";
+		if(j >=start+size_of_two && j <= start+size_of_two+size_of_one-1)
 		{
-			cout<<"j is in 2 segment"<<endl;
-			// traverse_seg_tree(i,j,start+size_of_two,start+size_of_two+size_of_one,a,d*3);	
+			cout<<"j in sec";
+			traverse_seg_tree(i,j,start+size_of_two,start+size_of_two+size_of_one-1,0,0);
 		}
-		if (start + size_of_two +size_of_one + size_of_zero <= j && start + size_of_two + size_of_one > j)			//j is in third segment
+		else if (j >=start+size_of_two+size_of_one && j <=end - 1 )
 		{
-			cout<<"j is in 3 segment"<<endl;
-			// traverse_seg_tree(i,start+size_of_two+size_of_one,start+size_of_two,start+size_of_two+size_of_one,a,d*3);	
-			// traverse_seg_tree(start+size_of_two+size_of_one,j,start+size_of_two+size_of_one,end);
+			cout<<"j in third";
+			traverse_seg_tree(i, start+size_of_two+size_of_one-1, start+size_of_two, start+size_of_two+size_of_one-1, 0, 0);	
+			traverse_seg_tree(start+size_of_two+size_of_one, j, start+size_of_two+size_of_one, end-1, 0, 0);	
+		
 		}
+
 	}
-	if (start+size_of_two+size_of_one <= i && i <end)
+	// i in third segment
+	if (i >=start+size_of_two+size_of_one && i <=end-1)
 	{
-		cout<<"i is in 3 segment"<<endl;
-		// traverse_seg_tree(i,j,start+size_of_two+size_of_one,a,)
+		cout<<"both in third";	
+		traverse_seg_tree(i,j, start+size_of_two+size_of_one,end - 1, 0,0);
 	}
 }
 int main(int argc, char const *argv[])
 {
 	int n;
 	cin>>n;
-	traverse_seg_tree(3,8,0,n,0,0);
+	int i,j;
+
+	traverse_seg_tree(i,j,0,n-1,0,1);
 	return 0;
 }
